117b1ba3f35e87ad7e9a031fc645552750d3ecc5
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-03-20 16:05:15 -0300

    Adding Image and Alt on alura-newsletter

--- Front-End/alura-newsletter/src/components/Article/index.jsx
+++ Front-End/alura-newsletter/src/components/Article/index.jsx
@@ -11,6 +11,8 @@ export default function Article({ title, text, tags, image, alt }) {
           </p>
         ))}
       </div>
+      {image && <img className="sm:p-4" src={image} />}
+      {image && alt && <span className="sr-only">{alt}</span>}
     </div>
   );
 }

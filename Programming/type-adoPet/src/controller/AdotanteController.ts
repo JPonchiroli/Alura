0eb23860f9661f7462ccbbd780b37e03867773d4
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-08-21 15:49:54 -0300

    Refactor criaAdontante for multiples errors

--- Programming/type-adoPet/src/controller/AdotanteController.ts
+++ Programming/type-adoPet/src/controller/AdotanteController.ts
@@ -28,12 +28,20 @@ export default class AdotanteController {
     let bodyValidated: TipoRequestBodyAdotante;
 
     try {
-      bodyValidated = await adotanteBodyValidator.validate(req.body)
-    } catch (error){
-      const yupErrors = error as yup.ValidationError
-      return res.status(400).json({error: yupErrors.message})
+      bodyValidated = await adotanteBodyValidator.validate(req.body, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      const yupErrors = error as yup.ValidationError;
+      const validationErros: Record<string, string> = {};
+
+      yupErrors.inner.forEach((error) => {
+        if (!error.path) return;
+        validationErros[error.path] = error.message;
+      });
+      return res.status(400).json({ error: validationErros });
     }
-    
+
     const novoAdotante = new AdotanteEntity(
       nome,
       senha,
